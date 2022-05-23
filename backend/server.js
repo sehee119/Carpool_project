72ae2a094309805acbd6301c245ee3e3eb91895b
신동진 <sdjin@shindongjin-ui-MacBookAir.local>
2022-05-23 21:57:47 +0900

    edit: postgres 연결정보

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const api_key = process.env.NCP_API_KEY;
 
 const QUERY_CARPOOL_LIST = `
 SELECT
-  name, gender, max_passenger, start_date, end_date, dotw, starting_point, start_coord, destination_point, destination_coord
+  name, gender, max_passenger, start_date, end_date, dotw, starting_point, starting_coord, destination_point, destination_coord
 FROM app_user 
 	join carpool on app_user.id = carpool.driver_id
 	join driver using(driver_id);
@@ -23,14 +23,20 @@ const options = {
   url: 'https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving',
   headers: { Accept: 'application/json', 'X-NCP-APIGW-API-KEY-ID' : api_key_id, 'X-NCP-APIGW-API-KEY' : api_key }
 }
-const db_client = new Client({
-  user: 'sdjin',
+// const db_client = new Client({ // 로컬
+//   user: 'sdjin',
+//   host: 'localhost',
+//   database: 'sdjin',
+//   password: 'tls888',
+//   port: 5432,
+// });
+const db_client = new Client({ // AWS 서버
+  user: 'postgres',
   host: 'localhost',
-  database: 'sdjin',
-  password: 'tls888',
+  database: 'carpool',
+  password: 'postgres',
   port: 5432,
 });
-
 
 async function main() {
   app.get('/list', (req, res) => { // 카풀 목록 불러오기
@@ -42,7 +48,7 @@ async function main() {
       db_client.end()
     });
   });
-  app.get('/filter', (req, res) => { // 카풀 검색(필터)하기
+  app.get('/filter', async (req, res) => { // 카풀 검색(필터)하기
     let start_date = req.query.start_date;
     let end_date = req.query.end_date;
     let dotw = req.query.dotw;
@@ -54,25 +60,18 @@ async function main() {
 
     // DB에서 카풀목록을 불러오고 추가적으로 소요시간을 계산하기 위해 좌표도 불러온다
     db_client.connect();
-    db_client.query(QUERY_CARPOOL_LIST, (error, results) => {
-      row = (error ? error.stack : results.rows)[0]
-      res.json(row)
-      db_client.end()
-    });
-    start = row['start_coord'];
-    goal = row['goal_coord'];
+    let result = await db_client.query(QUERY_CARPOOL_LIST);
+    row = result.rows[0]; // 모든 리스트에 대해 계산하도록 만들기
+    res.json(row);
+    await db_client.end();
 
-
-    getGeo('미추홀구 인하로 100')
-      .then(result => {
-        ride_spot = result;
-      });
-    getDuration(start, goal, ride_spot)
-      .then(result => {
-        duration = result;
-      });
+    start = row['starting_coord'];
+    goal = row['destination_coord'];
+    ride_spot = await getGeo('서울특별시 중구 통일로 13'); // 탑승자의 좌표를 인자로 받도록 만들기
+    old_duration = await getDuration(start, goal);
+    duration = await getDuration(start, goal, ride_spot);
     
-    
+    console.log('기존: %d, 경유지포함: %d', old_duration, duration);//
   });
 
   app.listen(3000, () => console.log('user connected?'));
