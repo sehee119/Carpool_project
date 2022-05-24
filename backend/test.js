cb22112f5139153312437a59f995e26641f3d539
sehee119 <75569474+sehee119@users.noreply.github.com>
2022-05-24 14:15:04 +0900

    Update test.js

--- backend/test.js
+++ backend/test.js
@@ -2,11 +2,20 @@ require('dotenv').config({ path: './.env' });
 
 const { getDuration } = require('./src/NCP/getDuration');
 const getGeo = require('./src/NCP/getGeo.js').getGeo;
+const transGeo = require('./src/NCP/transGeo.js').transGeo;
 
 async function test() {
-    let result = await getGeo('인천 부평구 광장로 16 부평민자역사');
+    //let result = await getGeo('인천 부평구 광장로 16 부평민자역사'); //인천 부평구 광장로 16 부평민자역사
+    let result = await transGeo('네이버 그린팩토리'); //인천 부평구 광장로 16 부평민자역사
+    //console.log("x, y "+ JSON.stringify(getGeo(result2)))
+    //console.log("1----" + result)
+    let result2 = await getGeo(result)
+    console.log(result2)
 
-    console.log('소요시간: %d', await getDuration('127.1058342,37.35970', '129.075986,35.179470', result))
+    //console.log('소요시간: %d', await getDuration('127.1058342,37.35970', '129.075986,35.179470', result));
+    //console.log('소요시간: %d', await getDuration('127.1058342,37.35970', '129.075986,35.179470', result2));
+
+
 }
 
 test('a', 'b', 'c', 'd', 'e');
