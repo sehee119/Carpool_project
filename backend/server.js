a453cdb18aeac69691ba522be6dd375892eb72fc
신동진 <sdjin@shindongjin-ui-MacBookAir.local>
2022-05-29 02:16:43 +0900

    fix: date타입 데이터, JSON형식 오류 수정 + code formatting

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const { getDuration } = require('./src/NCP/getDuration.js');
 
 const { Client } = require('pg');
 const express = require('express');
+const { transGeo } = require('./src/NCP/transGeo.js');
 const app = express();
 
 const api_key_id = process.env.NCP_API_KEY_ID;
@@ -16,128 +17,132 @@ SELECT
 FROM app_user 
 	join carpool on app_user.id = carpool.driver_id
 	join driver using(driver_id);
-`
+`;
 
 const options = {
   method: 'GET',
   url: 'https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving',
-  headers: { Accept: 'application/json', 'X-NCP-APIGW-API-KEY-ID' : api_key_id, 'X-NCP-APIGW-API-KEY' : api_key }
-}
-
+  headers: {
+    Accept: 'application/json',
+    'X-NCP-APIGW-API-KEY-ID': api_key_id,
+    'X-NCP-APIGW-API-KEY': api_key,
+  },
+};
 
 async function main() {
-  app.get('/list', (req, res) => { // 카풀 목록 불러오기
-    // const db_client = new Client({ // 로컬
-    //   user: 'sdjin',
-    //   host: 'localhost',
-    //   database: 'sdjin',
-    //   password: 'tls888',
-    //   port: 5432,
-    // });
+  // 카풀 목록 불러오기
+  app.get('/list', (req, res) => {
     const db_client = new Client({
+      // 로컬
       user: 'postgres',
-      host: 'localhost',
+      host: 'ec2-18-117-73-79.us-east-2.compute.amazonaws.com',
       database: 'carpool',
       password: 'postgres',
       port: 5432,
     });
+    // const db_client = new Client({
+    //   user: "postgres",
+    //   host: "localhost",
+    //   database: "carpool",
+    //   password: "postgres",
+    //   port: 5432,
+    // });
     res.header('Access-Control-Allow-Origin', '*'); // CORS
     let row;
     db_client.connect();
     db_client.query(QUERY_CARPOOL_LIST, (error, results) => {
-      row = (error ? error.stack : results.rows) // 카풀 목록 Object
-      res.json({ data : row })
-      db_client.end()
+      row = error ? error.stack : results.rows; // 카풀 목록 Object
+      res.json({ data: row });
+      db_client.end();
     });
   });
-  app.get('/filter', async (req, res) => { // 카풀 검색(필터)하기
-    // const db_client = new Client({ // 로컬
-    //   user: 'sdjin',
-    //   host: 'localhost',
-    //   database: 'sdjin',
-    //   password: 'tls888',
-    //   port: 5432,
-    // });
+  // 카풀 검색(필터)하기
+  app.get('/filter', async (req, res) => {
     const db_client = new Client({
+      // 로컬
       user: 'postgres',
-      host: 'localhost',
+      host: 'ec2-18-117-73-79.us-east-2.compute.amazonaws.com',
       database: 'carpool',
       password: 'postgres',
       port: 5432,
     });
+    // const db_client = new Client({
+    //   user: 'postgres',
+    //   host: 'localhost',
+    //   database: 'carpool',
+    //   password: 'postgres',
+    //   port: 5432,
+    // });
     res.header('Access-Control-Allow-Origin', '*'); // CORS
 
     let start_date = req.query.start_date;
     let end_date = req.query.end_date;
-    // let start_name = req.query.start_name;
-    let goal_name = req.query.goal_name;
-    let desired_time = req.query.desired_time;
+    // let desired_time = req.query.desired_time;
     // let ride_start_name = req.query.start_name; // 탑승지 // 주소말고 장소이름으로 가능하도록 바꿔야함
-    let ride_start_name = '서울특별시 중구 통일로 13';
+    let ride_start_name = req.query.start_name;
+    let ride_goal_name = req.query.goal_name;
     let gender = req.query.gender;
     let dotw = req.query.dotw;
-    
-    if(Array.isArray(gender) == false) gender = [gender];// 성별을 한개 택했을 때도 Array로 변경
-    if(Array.isArray(dotw) == false) dotw = [dotw];
-    
+
+    if (Array.isArray(gender) == false) gender = [gender]; // 성별을 한개 택했을 때도 Array로 변경
+    if (Array.isArray(dotw) == false) dotw = [dotw];
+
     const QUERY_CARPOOL_FILTER = `
     SELECT
-      name, gender, max_passenger, start_date, end_date, dotw, starting_point, starting_coord, destination_point, destination_coord
+      name, gender, max_passenger, start_date::text, end_date::text, dotw, starting_point, starting_coord, destination_point, destination_coord
     FROM app_user 
       join carpool on app_user.id = carpool.driver_id
       join driver using(driver_id)
-    WHERE gender = ANY(array[${gender.map(x => `'${x}'`).join(',')}]) and
-      dotw @> array[${dotw.map(x => `'${x}'`).join(',')}]
-    ;
-    `
-    // const QUERY_CARPOOL_FILTER = `
-    // SELECT
-    //   name, gender, max_passenger, start_date, end_date, dotw, starting_point, starting_coord, destination_point, destination_coord
-    // FROM app_user 
-    //   join carpool on app_user.id = carpool.driver_id
-    //   join driver using(driver_id)
-    // WHERE gender = ANY(array[${gender.map(x => `'${x}'`).join(',')}]) and
-    //   start_date <= '${start_date}' and end_date >= '${end_date}' and
-    //   dotw @> array[${dotw.map(x => `'${x}'`).join(',')}]
-    // ;
-    // `
+    WHERE gender = ANY($1) AND
+      (start_date <= $2 OR end_date >= $3) AND
+      dotw @> $4
+    ;`;
     // DB에서 카풀목록을 불러오고 추가적으로 소요시간을 계산하기 위해 좌표도 불러온다
     db_client.connect();
-    let result;
+    let db_result;
     try {
-      result = await db_client.query(QUERY_CARPOOL_FILTER);
-    } catch(error) {
+      db_result = await db_client.query(QUERY_CARPOOL_FILTER, [
+        gender,
+        start_date,
+        end_date,
+        dotw,
+      ]);
+    } catch (error) {
       res.send(error.message);
     }
-    await db_client.end(); // 비동기일 필요없음
-    let rows = result.rows;
-    console.log('쿼리 rows: ' + rows.length);
-    console.log(QUERY_CARPOOL_FILTER);
-    let data = [];
-
-    if(rows.length == 0) {
-      res.send([]);// 검색결과가 없는 경우
-    } else {
-      for await (const row of rows) {
-        let start = row['starting_coord'];
-        let goal = row['destination_coord'];
-        let ride_spot = await getGeo(ride_start_name);
-        let old_duration = await getDuration(start, goal);
-        let duration = await getDuration(start, goal, ride_spot);
-        
-        data.push({row,
-          ride_time: 0,
-          time_difference : duration - old_duration,
-          distance_difference: 0,
-        });
-        res.json({data: data});// 결과 전송
-      }
+    await db_client.end();
+    if (db_result == null) {
+      res.send({ data: [] }); // 검색결과가 없는 경우
+      return;
     }
+    let rows = db_result.rows;
+    let search_data = [];
+
+    for await (const row of rows) {
+      let start = row['starting_coord'];
+      let goal = row['destination_coord'];
+      let ride_spot = await getGeo(transGeo(ride_start_name));
+      let old_duration = await getDuration(start, goal);
+      let duration = await getDuration(start, goal, ride_spot);
+
+      console.log(
+        '이전 소요시간: %d, 현재 소요시간: %d',
+        old_duration,
+        duration
+      ); //
+      search_data.push({
+        ...row,
+        ride_time: 0,
+        time_difference: duration - old_duration,
+        distance_difference: 0,
+      });
+    }
+    res.json({ data: search_data }); // 결과 전송 to client
   });
-  app.get('/register', async (req, res) => { // 카풀 등록하기
-    
+  app.get('/register', async (req, res) => {
+    // 카풀 등록하기
   });
   app.listen(3000, () => console.log('user connected?'));
 }
 
-main()
+main();
