c0289e6eff61e436004d4c52379ce10b15d2dbe0
신동진 <sdjin@shindongjin-ui-MacBookAir.local>
2022-05-29 02:19:09 +0900

    fix: 검색결과 없는 경우, undefined exception 추가

--- backend/src/NCP/getGeo.js
+++ backend/src/NCP/getGeo.js
@@ -1,28 +1,32 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const api_key_id = process.env.NCP_API_KEY_ID;
 const api_key = process.env.NCP_API_KEY;
 
 async function getGeo(name) {
-    const url = `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=${encodeURIComponent(name)}`;
-    const options = {
-        method: 'GET',
-        url: url,
-        // url: url + '?query=' + encodeURIComponent(name),
-        // params: { query: encodeURIComponent(name) },
-        headers: { Accept: 'application/json', 'X-NCP-APIGW-API-KEY-ID' : api_key_id, 'X-NCP-APIGW-API-KEY' : api_key }
-    }
-    let res;
-    try {
-        res = await axios(options);
-    } catch(err) {
-        console.error('Geo 정보를 불러오지 못했습니다.');
-    }
-    let address = res.data.addresses[0];
-    console.log('쿼리 주소: ' + name);//
+  const url = `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=${encodeURIComponent(
+    name
+  )}`;
+  const options = {
+    method: "GET",
+    url: url,
+    // url: url + '?query=' + encodeURIComponent(name),
+    // params: { query: encodeURIComponent(name) },
+    headers: {
+      Accept: "application/json",
+      "X-NCP-APIGW-API-KEY-ID": api_key_id,
+      "X-NCP-APIGW-API-KEY": api_key,
+    },
+  };
+  let res;
+  try {
+    res = await axios(options);
+  } catch (err) {
+    console.error("Geo 정보를 불러오지 못했습니다.");
+  }
+  let address = res.data.address ? res.data.addresses[0] : { x: 0, y: 0 };
 
-    return address.x + ',' + address.y;
+  return address.x + "," + address.y;
 }
 
 exports.getGeo = getGeo;
-
