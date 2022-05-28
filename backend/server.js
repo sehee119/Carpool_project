ffb2b5ff3f82602212600eec852ca79a8de232c0
신동진 <sdjin@shindongjin-ui-MacBookAir.local>
2022-05-29 05:33:33 +0900

    feat: register() 카풀등록 구현

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 require('dotenv').config({ path: './.env' });
 
-const { getGeo } = require('./src/NCP/getGeo.js');
-const { getDuration } = require('./src/NCP/getDuration.js');
-
 const { Client } = require('pg');
 const express = require('express');
-const { transGeo } = require('./src/NCP/transGeo.js');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const { getGeo } = require('./src/NCP/getGeo.js');
+const { getDuration } = require('./src/NCP/getDuration.js');
+const { transGeo } = require('./src/NCP/transGeo.js');
 
 const QUERY_CARPOOL_LIST = `
 SELECT
@@ -25,9 +28,20 @@ WHERE gender = ANY($1) AND
   (start_date <= $2 OR end_date >= $3) AND
   dotw @> $4
 ;`;
-
+const INSERT_CARPOOL_NEW = `
+WITH new_carpool AS (
+  INSERT INTO carpool VALUES(
+    default, $1, $2, $3, $4, 
+      $5, $6,
+      $7, $8, 
+      $9, $10)
+  RETURNING driver_id)
+INSERT INTO driver 
+  (SELECT driver_id, $11, $12, 0 FROM new_carpool)
+  ON CONFLICT DO NOTHING;    
+`;
+// 카풀 목록 불러오기
 async function main() {
-  // 카풀 목록 불러오기
   app.get('/list', (req, res) => {
     // const db_client = new Client({
     //   // 로컬
@@ -38,10 +52,10 @@ async function main() {
     //   port: 5432,
     // });
     const db_client = new Client({
-      user: "postgres",
-      host: "localhost",
-      database: "carpool",
-      password: "postgres",
+      user: 'postgres',
+      host: 'localhost',
+      database: 'carpool',
+      password: 'postgres',
       port: 5432,
     });
     res.header('Access-Control-Allow-Origin', '*'); // CORS
@@ -49,7 +63,9 @@ async function main() {
     db_client.connect();
     db_client.query(QUERY_CARPOOL_LIST, (error, results) => {
       row = error ? error.stack : results.rows; // 카풀 목록 Object
-      res.json({ data: row });
+      res
+        .status(200)
+        .json({ status: 'success', message: '목록 불러오기 성공', data: row });
       db_client.end();
     });
   });
@@ -94,7 +110,9 @@ async function main() {
         dotw,
       ]);
     } catch (error) {
-      res.send(error.message);
+      console.log(error.message);
+      res.status(400).json({ status: 'error', message: error.message }); // 결과 전송 to client
+      return;
     }
     await db_client.end();
 
@@ -120,11 +138,73 @@ async function main() {
         distance_difference: 0,
       });
     }
-    res.json({ data: search_data }); // 결과 전송 to client
+    res
+      .status(200)
+      .json({
+        status: 'success',
+        message: '목록 불러오기 성공',
+        data: search_data,
+      }); // 결과 전송 to client
   });
-  app.get('/register', async (req, res) => {
-    // 카풀 등록하기
+  // 카풀 등록하기
+  app.post('/register', async (req, res) => {
+    let driver_name = req.body.name; // 이미 알고있음
+    let driver_gender = req.body.gender; // 이미 알고있음
+    let max_passenger = req.body.max_passenger;
+    let start_date = req.body.start_date;
+    let end_date = req.body.end_date;
+    let dotw = req.body.dotw;
+    let starting_point = req.body.start_name;
+    let destination_point = req.body.goal_name;
+    let desried_arrival_time = req.body.arrival_time;
+    let car_num = req.body.car_num;
+    let car_category = req.body.car_category;
+
+    if (Array.isArray(dotw) == false) dotw = [dotw];
+
+    // const db_client = new Client({
+    //   // 로컬
+    //   user: 'postgres',
+    //   host: 'ec2-18-117-73-79.us-east-2.compute.amazonaws.com',
+    //   database: 'carpool',
+    //   password: 'postgres',
+    //   port: 5432,
+    // });
+    const db_client = new Client({
+      user: 'postgres',
+      host: 'localhost',
+      database: 'carpool',
+      password: 'postgres',
+      port: 5432,
+    });
+    let db_result = {};
+    await db_client.connect();
+    try {
+      db_result = await db_client.query(INSERT_CARPOOL_NEW, [
+        req.body.client_id, // 클라이언트의 DB의 실제 id
+        start_date,
+        end_date,
+        dotw,
+        starting_point,
+        getGeo(transGeo(starting_point)),
+        destination_point,
+        getGeo(transGeo(destination_point)),
+        desried_arrival_time,
+        max_passenger,
+        car_category,
+        car_num,
+      ]);
+    } catch (error) {
+      console.log(error.message);
+      res.status(400).json({ status: 'error', message: error.message });
+      return;
+    }
+    await db_client.end();
+    console.log('새로운 카풀등록');
+    res.status(200).json({ status: 'success', message: '등록되었습니다.' });
   });
+  // 카풀 신청하기
+  app.get('/candidate', async (req, res) => {});
   app.listen(3000, () => console.log('user connected?'));
 }
 
