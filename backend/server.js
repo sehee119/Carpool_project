c2e5cb2e2f84f94e0e63d36d6f39957ae072418d
신동진 <sdjin@shindongjin-ui-MacBookAir.local>
2022-06-13 22:16:06 +0900

    fix: /list by id SQL

--- backend/server.js
+++ backend/server.js
@@ -49,10 +49,14 @@ ORDER BY created
 ;`;
 const QUERY_CARPOOL_LIST_BY_ID = `
 SELECT
-	carpool.id as carpool_id, name, gender, max_passenger, start_date, end_date, dotw, starting_point, destination_point, desired_arrival_time
+	carpool.id as carpool_id, name, gender, max_passenger, 
+	candidate.start_date, candidate.end_date, candidate.dotw, 
+	starting_point, destination_point, desired_arrival_time,
+	ride_spot, is_accepted
 FROM app_user 
 	join carpool on app_user.id = carpool.driver_id
 	join driver using(driver_id)
+  	join candidate on carpool.id = candidate.carpool_id
 WHERE app_user.id = $1
 ;`;
 const QUERY_CARPOOL_FILTER = `
